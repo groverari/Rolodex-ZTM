3574443e953a31ccdfce28e9b0db20e7c9117295
Ariesh Grover <41345908+groverari@users.noreply.github.com>
2023-04-06 22:03:03 -0700

    Finished app with class components

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           className="main-search"
           placeholder="search monsters"
